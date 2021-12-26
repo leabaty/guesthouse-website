5c8b77a8ecda7130c47bfb8ebedafc5bb67b56d9
Léa Baty <leabaty.dev@gmail.com>
2021-12-26 21:35:45 +0100

    hamburger menu original setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { GiKeyring } from "react-icons/gi";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <div className="navbar">
@@ -13,7 +17,9 @@ function Navbar() {
             <GiKeyring className="navbar-icon" />
             Maison d'hôtes
           </Link>
-          <div className="menu-icon"></div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <HiOutlineMenuAlt3 />}
+          </div>
         </div>
       </div>
     </>
