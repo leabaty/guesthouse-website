79acc1b6a6c70ab8f331d1d93ab566851272ce85
Léa Baty <leabaty.dev@gmail.com>
2021-12-27 17:19:09 +0100

    final minor CSS fixes to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
@@ -22,14 +22,17 @@ function Navbar() {
     }
   };
   window.addEventListener("resize", showButton);
-  // Avec cette condition, on vérifie la taille du navigateur avec un eventlistener
-  // et on détermine si le bouton doit être visible ou non en changeant sa valeur dans le state.
+  // Avec cette condition, on vérifie la taille du navigateur avec un useEffect (qui n'intervient qu'une fois, au démarrage du site)
+  useEffect(() => {
+    showButton();
+  }, []);
+  // et cela va donc déterminer si le bouton doit être visible ou non en changeant sa valeur dans le state.
 
   return (
     <>
       <div className="navbar">
         <div className="navbar-container container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             <GiKeyring className="navbar-icon" />
             Domaine de BSM
           </Link>
@@ -38,25 +41,33 @@ function Navbar() {
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/chambres" className="nav-links">
+              <Link
+                to="/chambres"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Chambres
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link to="/table" className="nav-links">
+              <Link to="/table" className="nav-links" onClick={closeMobileMenu}>
                 Table
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link to="/tarifs" className="nav-links">
+              <Link
+                to="/tarifs"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Tarifs et disponibilités
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link to="/acces" className="nav-links">
+              <Link to="/acces" className="nav-links" onClick={closeMobileMenu}>
                 Accès
               </Link>
             </li>
@@ -67,7 +78,11 @@ function Navbar() {
                   <Button buttonStyle="btn--outline">Contact</Button>
                 </Link>
               ) : (
-                <Link to="/contact" className="btn-link">
+                <Link
+                  to="/contact"
+                  className="btn-link"
+                  onClick={closeMobileMenu}
+                >
                   <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
                     Contact
                   </Button>
