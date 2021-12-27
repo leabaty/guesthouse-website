b60f9cc8e94bb12981ebf844656174b6e2bd5032
Léa Baty <leabaty.dev@gmail.com>
2021-12-27 12:28:20 +0100

    button css and mobile setup

---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Button.css";
+
+const STYLE = ["btn--full", "btn--outline"];
+
+const SIZE = ["btn--medium", "btn--large", "btn--mobile"];
+
+const COLOR = ["primary", "red", "green"]; 
+
+export const Button =({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize, 
+    buttonColor
+
+}) => {
+
+    const checkButtonStyle = STYLE.includes (buttonStyle) ? buttonStyle : STYLE[0]
+    const checkButtonSize = SIZE.includes (buttonSize) ? buttonSize : SIZE[0]
+    const checkButtonColor = COLOR.includes (buttonColor) ? buttonColor : COLOR[0]
+
+    return (
+        <button 
+        /*1*/ className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} 
+        /*2*/onClick={onClick} type={type}
+        /*3*/>{children}</button>
+        )
+
+        // 1 - Il ajoute les classes par défaut (position 0 du tableau) si il n'y a pas de classe de définie spécifiquement.
+        // 2 - OnClick et type sont le event listener et les propriétés du bouton qui sont générées grâce au fait de passer ces props ici
+        // 3 - Children correspond à ce qu'on va noter dans l'intitulé du bouton ("Contact"). 
+
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { GiKeyring } from "react-icons/gi";
+import { Button } from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  // Par défaut, le bouton dans le state est sur "true".
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener("resize", showButton);
+  // Avec cette condition, on vérifie la taille du navigateur avec un eventlistener
+  // et on détermine si le bouton doit être visible ou non en changeant sa valeur dans le state.
 
   return (
     <>
@@ -20,6 +35,45 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             {click ? <FaTimes /> : <HiOutlineMenuAlt3 />}
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/chambres" className="nav-links">
+                Chambres
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/table" className="nav-links">
+                Table
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/tarifs" className="nav-links">
+                Tarifs et disponibilités
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to="/acces" className="nav-links">
+                Accès
+              </Link>
+            </li>
+
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/contact" className="btn-link">
+                  <Button buttonStyle="btn--outline">Contact</Button>
+                </Link>
+              ) : (
+                <Link to="/contact" className="btn-link">
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    Contact
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
